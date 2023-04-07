6de425466646f2881561e27181b2a762d4e65347
Ankur-Vekariya <ankurvekaariya2001@gmail.com>
2023-04-07 23:42:44 +0530

    Hotels all api created

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -15,8 +15,49 @@ router.post("/", async (req, res) => {
   }
 });
 //update
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //get all
+
+router.get("/", async (req, res, next) => {
+  console.log("im in get all hotel");
+  next();
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
