16f3066a9261492efd5e3a08efa20a2b73de9114
Ankur-Vekariya <ankurvekaariya2001@gmail.com>
2023-04-09 00:01:04 +0530

    Created controllers for hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,34 +1,14 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { createHotel, updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
 //create
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
-
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post("/", createHotel);
 //update
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", updateHotel);
 //delete
 router.delete("/:id", async (req, res) => {
   try {
@@ -50,13 +30,12 @@ router.get("/:id", async (req, res) => {
 //get all
 
 router.get("/", async (req, res, next) => {
-  console.log("im in get all hotel");
-  next();
   try {
     const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (error) {
     res.status(500).json(error);
+    next(error);
   }
 });
 
